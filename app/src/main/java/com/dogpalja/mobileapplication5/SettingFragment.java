1b7b1b795a5af9daf8a507b2bff31e72db25ee1d
mimseong <parkseongmin98@gmail.com>
2019-11-22 13:41:31 +0900

    Revert "SettingFragment.java파일 버튼 클릭시 작동할 뼈대 만듬" 버그 생겨서 다시 돌려놓음
    This reverts commit 7add95d35bc4b772bca6688e663a3a6c2e138419.

--- app/src/main/java/com/dogpalja/mobileapplication5/SettingFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/SettingFragment.java
@@ -4,20 +4,15 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 
 public class SettingFragment extends Fragment {
-
-    TextView btn_contact_us, btn_developer_info, btn_app_info, btn_cheer_up;
 
     public SettingFragment() {
         // Required empty public constructor
@@ -32,49 +27,8 @@ public class SettingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_camera, container, false);
-
-        btn_contact_us = (TextView) view.findViewById(R.id.btn_contact_us);
-        btn_developer_info = (TextView) view.findViewById(R.id.btn_developer_info);
-        btn_app_info = (TextView) view.findViewById(R.id.btn_app_info);
-        btn_cheer_up = (TextView) view.findViewById(R.id.btn_cheer_up);
-
-        return view;
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false);
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        btn_contact_us.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext() , "btn_contact_us", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btn_developer_info.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext() , "btn_developer_info", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btn_app_info.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext() , "btn_app_info", Toast.LENGTH_LONG).show();
-            }
-        });
-
-        btn_cheer_up.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext() , "btn_cheer_up", Toast.LENGTH_LONG).show();
-            }
-        });
-
-
-    }
 }
