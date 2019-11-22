630693c6192cef8dd3f006b5fa6e900d81e2ae78
mimseong <parkseongmin98@gmail.com>
2019-11-22 14:50:05 +0900

    건강체크 연결

--- app/src/main/java/com/dogpalja/mobileapplication5/CameraFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/CameraFragment.java
@@ -1,23 +1,19 @@
 package com.dogpalja.mobileapplication5;
 
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.content.pm.PackageManager;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,10 +23,10 @@ import static android.app.Activity.RESULT_OK;
 
 public class CameraFragment extends Fragment {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
     Button upload_btn, capture_btn;
     ImageView captured_iv;
-    Uri mImageUri;
-    Bitmap bitmap;
 
     final int CAPTURE_IMAGE = 1;
 
@@ -54,7 +50,6 @@ public class CameraFragment extends Fragment {
         capture_btn = (Button) view.findViewById(R.id.capture_btn);
         captured_iv = (ImageView) view.findViewById(R.id.captured_iv);
 
-
         return view;
     }
 
@@ -63,60 +58,16 @@ public class CameraFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        capture_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                capturePhoto();
-            }
-        });
-
-        upload_btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                imageTitle();
-            }
-        });
-
     }
 
-    public void capturePhoto() {
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        String imageName = "image.jpg";
-        mImageUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), imageName));
-        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri);
-        startActivityForResult(cameraIntent, CAPTURE_IMAGE);
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
 
-        if(requestCode == CAPTURE_IMAGE){
-            if(resultCode == RESULT_OK){
-                if(mImageUri != null){
-
-                    //convert uri to bitmap
-                    try {
-                        bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), mImageUri);
-
-                        if(bitmap != null){ //null이 아닐 시 captured_iv 이미지 변경
-                            captured_iv.setImageBitmap(bitmap);
-                            Log.i("bitmap", bitmap.toString());
-                        }
-
-                    } catch (IOException e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-    }
-
-    public void imageTitle(){
-
-    }
 
 
 }
