97dd831b0390eda86fdecad4ea1ca13a0402fb1d
mimseong <parkseongmin98@gmail.com>
2019-11-24 19:16:04 +0900

    CameraFragment.java 버그로 인해 다시 초기화

--- app/src/main/java/com/dogpalja/mobileapplication5/CameraFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/CameraFragment.java
@@ -24,9 +24,6 @@ public class CameraFragment extends Fragment {
 
     private static final int REQUEST_IMAGE_CAPTURE = 101;
 
-    Button moment_camera_btn, essay_camera_btn;
-    ImageView captured_iv;
-
 
     public CameraFragment() {
         // Required empty public constructor
@@ -44,59 +41,8 @@ public class CameraFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
-//        moment_camera_btn = (Button) view.findViewById(R.id.moment_camera_btn);
-//        essay_camera_btn = (Button) view.findViewById(R.id.essay_camera_btn);
-//
-//        captured_iv = (ImageView) view.findViewById(R.id.captured_iv);
-
-        //button click
-        moment_camera_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //if system version >= marshmallow, request runtime permission
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    //if(che)
-                }
-
-
-            }
-        });
 
         return view;
     }
-
-    public void takePicture(View view){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if(takePictureIntent.resolveActivity(getContext().getPackageManager()) != null){
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            captured_iv.setImageBitmap(imageBitmap);
-        }
-    }
-
-
-    //Call after view is created
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-
-//    private void dispatchTakePictureIntent() {
-//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-//        }
-//    }
-
-
 
 }
