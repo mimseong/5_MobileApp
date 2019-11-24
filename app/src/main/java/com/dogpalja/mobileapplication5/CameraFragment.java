2f3be804535070a5dfccd374698b27f602a4d9cc
mimseong <parkseongmin98@gmail.com>
2019-11-24 20:25:04 +0900

    CameraFragment.java 카메라 촬영, 미리보기 이미지 가져오는 함수 추가

--- app/src/main/java/com/dogpalja/mobileapplication5/CameraFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/CameraFragment.java
@@ -2,26 +2,22 @@ package com.dogpalja.mobileapplication5;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.os.Build;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import static android.app.Activity.RESULT_OK;
 
 
-
 public class CameraFragment extends Fragment {
 
+    private ImageView imageView;
     private static final int REQUEST_IMAGE_CAPTURE = 101;
 
 
@@ -38,11 +34,35 @@ public class CameraFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        //기존의 코드
+        //View view = inflater.inflate(R.layout.fragment_camera, container, false);
 
-        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        //테스트용으로 만든 xml파일 임시로 붙임
+        View view = inflater.inflate(R.layout.test_camera, container, false);
 
+        imageView = view.findViewById(R.id.imageView);
 
         return view;
+    }
+
+    //카메라 앱으로 사진 촬영
+    public void takePicture(View view){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //미리보기 이미지 가져오기
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
     }
 
 }
