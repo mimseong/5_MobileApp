43379eac72ece16a67ecffb2e1503cfe4fac14fb
mimseong <parkseongmin98@gmail.com>
2019-11-25 10:57:06 +0900

    원본 사진 보여주는 함수 추가
    - 파일 경로로 저장
    - DisplayImage 사용

--- app/src/main/java/com/dogpalja/mobileapplication5/CameraFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/CameraFragment.java
@@ -1,11 +1,18 @@
 package com.dogpalja.mobileapplication5;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,13 +20,22 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static android.app.Activity.RESULT_OK;
 
 
 public class CameraFragment extends Fragment {
 
-    private ImageView imageView;
     private Button capture_btn;
+    private Button show_img_btn;
+
+    String currentPhotoPath = null;
+
+
     private static final int REQUEST_IMAGE_CAPTURE = 1;
 
 
@@ -42,7 +58,7 @@ public class CameraFragment extends Fragment {
         //테스트용으로 만든 xml파일 임시로 붙임
         View view = inflater.inflate(R.layout.test_camera, container, false);
 
-        imageView = (ImageView) view.findViewById(R.id.capture_iv);
+        show_img_btn = (Button) view.findViewById(R.id.show_img_btn);
         capture_btn = (Button) view.findViewById(R.id.capture_btn);
 
         capture_btn.setOnClickListener(new View.OnClickListener() {
@@ -53,27 +69,61 @@ public class CameraFragment extends Fragment {
             }
         });
 
+        show_img_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cInt = new Intent(getContext(), DisplayImgActivity.class);
+                cInt.putExtra("image_path", currentPhotoPath);
+                startActivity(cInt);
+            }
+        });
+
         return view;
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "jpg_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
     //카메라 앱으로 사진 촬영
     public void takePicture(View view){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(getContext(),
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+
+
         }
     }
 
-    //미리보기 이미지 가져오기
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        //super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
-        }
-    }
+
+
+
 
 }
