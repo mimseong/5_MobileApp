4938fa65e12bf73d8183798e4cc8e4c94a694ffc
choo <fly7283@naver.com>
2019-11-29 03:34:30 +0900

    스토리맵연결

--- app/src/main/java/com/dogpalja/mobileapplication5/MapFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/MapFragment.java
@@ -27,7 +27,7 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        return inflater.inflate(R.layout.activity_story_map, container, false);
     }
 
 }
--- app/src/main/java/com/dogpalja/mobileapplication5/StoryMap.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/StoryMap.java
@@ -1,19 +1,14 @@
 package com.dogpalja.mobileapplication5;
-
-
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.location.Address;
-import android.location.Geocoder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.ViewGroup;
 import android.widget.Toast;
-
 import androidx.fragment.app.FragmentActivity;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,23 +17,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
-import java.util.List;
-
-public class StoryMap extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener {
+public class StoryMap extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener{
 
     private GoogleMap mMap;
-    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("clis", "맵 생성");
         setContentView(R.layout.activity_story_map);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.storymap);
         mapFragment.getMapAsync(this);
-        /////////////////////////////////////////
     }
 
     @Override
@@ -58,34 +49,64 @@ public class StoryMap extends FragmentActivity implements OnMapReadyCallback, Go
                         .setPositiveButton("네", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                Log.i("cli", "마커 추가");
+                                Log.i("clis", "마커 추가");
                                 googleMap.addMarker(mOptions); // 마커 추가.
                                 Toast.makeText(StoryMap.this, "추가 하였습니다.", Toast.LENGTH_SHORT).show();
                             }
                         }).setNegativeButton("아니오", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        Log.i("clis", "클릭했습니다.");
                         Toast.makeText(StoryMap.this, "추가 하지 않았습니다.", Toast.LENGTH_SHORT).show();
                     }
                 }).create().show();
             }
         });
+
         mMap.setOnMarkerClickListener(this); //정보창 클릭 리스너
         mMap.setOnInfoWindowClickListener(this); // 마커 클릭 리스너
         ////카메라 줌부분.
         LatLng knu = new LatLng(35.89, 128.61);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(knu, 10));
+
     } //mapready 마지막 부분.
 
     //마커 클릭 이벤트
     @Override
     public boolean onMarkerClick(Marker marker) {
-        Log.i("cli", "마커 클릭");
+        Log.i("clis", "마커 클릭");
         return false;
     }
+
     //정보창 클릭 이벤트
     @Override
     public void onInfoWindowClick(Marker marker) {
-    Log.i("cli", "정보창 클릭");
+        Context mContext = getApplicationContext();
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+        //R.layout.dialog는 xml 파일명이고  R.id.popup은 보여줄 레이아웃 아이디
+        View layout = inflater.inflate(R.layout.fragment_map,(ViewGroup) findViewById(R.id.popup));
+        AlertDialog.Builder aDialog = new AlertDialog.Builder(this);
+
+        aDialog.setTitle("대표 사진"); //타이틀바 제목
+        aDialog.setView(layout); //dialog.xml 파일을 뷰로 셋팅
+        //그냥 닫기버튼을 위한 부분
+        aDialog.setNegativeButton("닫기", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+
+        //저 부분에 사진 추가 할부분~~
+        aDialog.setNeutralButton("사진추가", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        //팝업창 생성
+        AlertDialog ad = aDialog.create();
+        ad.show();//보여줌!
+    Log.i("clis", "정보창 클릭");
     }
+
 }
