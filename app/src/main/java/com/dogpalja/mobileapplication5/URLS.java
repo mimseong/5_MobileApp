4ee245f73fa262321e356cfa66b2e88098ce038f
mimseong <parkseongmin98@gmail.com>
2019-12-04 21:45:50 +0900

    데이터베이스 url 추가

--- app/src/main/java/com/dogpalja/mobileapplication5/URLS.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/URLS.java
@@ -1,6 +1,6 @@
 package com.dogpalja.mobileapplication5;
 
 public class URLS {
-    static final String login_api = "";
-    static final String sign_up_api = "";
+    static final String login_api = "https://mobileapp5.000webhostapp.com/login.php";
+    static final String sign_up_api = "https://mobileapp5.000webhostapp.com/sign_up.php";
 }
