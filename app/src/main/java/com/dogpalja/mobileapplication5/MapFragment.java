6916b1f83a9955279aa46cda06193f98e484292f
choo <fly7283@naver.com>
2019-12-05 19:59:00 +0900

    스토리맵 마커삭제

--- app/src/main/java/com/dogpalja/mobileapplication5/MapFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/MapFragment.java
@@ -3,6 +3,7 @@ package com.dogpalja.mobileapplication5;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -26,7 +27,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener, GoogleMap.OnInfoWindowClickListener {
+public class MapFragment extends Fragment implements OnMapReadyCallback{
     public static View rootView;
     public static MapView mapView;
     public static GoogleMap mMap;
@@ -96,48 +97,72 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
             @Override
             public void onMapClick(LatLng point) {
                 final MarkerOptions mOptions = new MarkerOptions();
-                // 마커 타이틀
+            // 마커 타이틀
                 mOptions.title("대표사진보기");
                 mOptions.position(new LatLng(point.latitude, point.longitude));
+            // 마커(핀) 추가 , 맵 클릭시 확인창 띄우기
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MapFragment.this.getActivity());
+                dialog.setTitle("가봤던 곳").setMessage("핀을 추가 하시겠습니까?")
+                        .setPositiveButton("네", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i("clis", "마커 추가");
+                    googleMap.addMarker(mOptions); // 마커 추가.
+                    Toast.makeText(MapFragment.this.getActivity(), "추가 하였습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }).setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i("clis", "클릭했습니다.");
+                    Toast.makeText(MapFragment.this.getActivity(), "추가 하지 않았습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }).create().show();
+        }
+        });
+
+//정보창 클릭 리스너
+        googleMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+            
+                Log.i("clis", "정 보창 클릭");
+            }
+        });
+
+//마커 클릭 리스너
+        googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(final Marker marker) {
+                //삭제 할껀지... 여쭈어봄..
+                final MarkerOptions mOptions = new MarkerOptions();
+                // 마커 타이틀
+                mOptions.title("삭제");
                 // 마커(핀) 추가 , 맵 클릭시 확인창 띄우기
                 AlertDialog.Builder dialog = new AlertDialog.Builder(MapFragment.this.getActivity());
-                dialog.setTitle("가봤던 곳").setMessage("핀을 추가 하시겠습니까?")
+                dialog.setTitle("여쭈어봄").setMessage("삭제하시겠습니까?")
                         .setPositiveButton("네", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 Log.i("clis", "마커 추가");
-                           googleMap.addMarker(mOptions); // 마커 추가.
-                                Toast.makeText(MapFragment.this.getActivity(), "추가 하였습니다.", Toast.LENGTH_SHORT).show();
+                                marker.remove(); // 마커 삭제.
+                                Toast.makeText(MapFragment.this.getActivity(), "삭제 하였습니다.", Toast.LENGTH_SHORT).show();
                             }
                         }).setNegativeButton("아니오", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Log.i("clis", "클릭했습니다.");
-                        Toast.makeText(MapFragment.this.getActivity(), "추가 하지 않았습니다.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MapFragment.this.getActivity(), "삭제 하지 않았습니다.", Toast.LENGTH_SHORT).show();
                     }
                 }).create().show();
+                Log.i("clis", "마커 클릭");
+                return false;
             }
         });
 
-//        mMap.setOnMarkerClickListener((GoogleMap.OnMarkerClickListener) MapFragment.this.getActivity()); //정보창 클릭 리스너
-//        mMap.setOnInfoWindowClickListener((GoogleMap.OnInfoWindowClickListener) MapFragment.this.getActivity()); // 마커 클릭 리스너
-        ////카메라 줌부분.
-     //   LatLng knu = new LatLng(35.89, 128.61);
-     //   mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(knu, 10));
-    //    googleMap.addMarker(new MarkerOptions()
-      //          .position(new LatLng(35.141233, 126.925594))
-        //        .title("루프리코리아"));
 
     }
 
-    @Override
-    public void onInfoWindowClick(Marker marker) {
-        Log.i("clis", "정보창 클릭");
-    }
 
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        Log.i("clis", "마커 클릭");
-        return false;
-    }
+
+
 }
