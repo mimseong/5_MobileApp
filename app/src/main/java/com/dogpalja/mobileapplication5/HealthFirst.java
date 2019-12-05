513cafe875d27e84b07e34b68d0af3f1ec30cd67
gowithmong <gowithmong@naver.com>
2019-12-06 03:13:09 +0900

    건강부분, 세부 변수 설정

--- app/src/main/java/com/dogpalja/mobileapplication5/HealthFirst.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/HealthFirst.java
@@ -1,4 +1,24 @@
 package com.dogpalja.mobileapplication5;
 
-public class HealthFirst {
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HealthFirst extends AppCompatActivity {
+
+    SharedPreferences shared = getPreferences(Context.MODE_PRIVATE);
+    EditText name, vaccin1_d, vaccin2_d, vaccin3_d, vaccin_d, heart_d, rabies_d, now_w, prev_w;
+    //d는 날짜 입력.
+    Button name_b, vaccin1_c, vaccin2_c, vaccin3_c, vaccin4_c, heart_c, rabies_c, id_c, insert_w, vomit_c, dia_c, nasal_c;
+    //b는 버튼, c는 체크박스
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.health_first);
+    }
 }
