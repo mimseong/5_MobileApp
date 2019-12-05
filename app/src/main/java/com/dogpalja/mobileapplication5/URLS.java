1e818dc1aa1311ce98edbc8c16fa23cd382868cf
mimseong <parkseongmin98@gmail.com>
2019-12-05 19:27:40 +0900

    이미지 올릴 디비 URL 추가

--- app/src/main/java/com/dogpalja/mobileapplication5/URLS.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/URLS.java
@@ -3,4 +3,6 @@ package com.dogpalja.mobileapplication5;
 public class URLS {
     static final String login_api = "https://mobileapp5.000webhostapp.com/login.php";
     static final String sign_up_api = "https://mobileapp5.000webhostapp.com/sign_up.php";
+    static final String upload_story_image = "https://mobileapp5.000webhostapp.com/upload_story_image.php";
+    static final String get_user_data = "https://mobileapp5.000webhostapp.com/get_user_data.php?user_id=";
 }
