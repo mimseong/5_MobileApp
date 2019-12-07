4e0f9c6a1772e30bccd83ab0ca573a2248a093d8
mimseong <parkseongmin98@gmail.com>
2019-12-07 10:20:14 +0900

    환경설정 추가, 화면 간 이동 가능!

--- app/src/main/java/com/dogpalja/mobileapplication5/SettingFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/SettingFragment.java
@@ -4,15 +4,21 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 
 
 public class SettingFragment extends Fragment {
+
+    NavigationView mNavigationView;
 
     public SettingFragment() {
         // Required empty public constructor
@@ -28,7 +34,43 @@ public class SettingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
-    }
+        View view =  inflater.inflate(R.layout.fragment_setting, container, false);
+        mNavigationView = view.findViewById(R.id.setting_nav_view);
+        mNavigationView.setNavigationItemSelectedListener(navListner);
 
+        return view;
+    }
+    private NavigationView.OnNavigationItemSelectedListener navListner =
+        new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                ///in bottom navigation view
+                if(item.getItemId() == R.id.contact_us){
+                    fragment = new ContactFragment();
+
+                }
+                else if(item.getItemId() == R.id.developer_info){
+                    fragment = new DeveloperFragment();
+
+                }
+                else if(item.getItemId() == R.id.app_info){
+                    fragment = new AppinfoFragment();
+
+                }
+                else if(item.getItemId() == R.id.cheer_up){
+                    fragment = new CheerUpFragment();
+
+                }
+                else {
+                    Toast.makeText(getContext(),"Error",Toast.LENGTH_LONG).show();
+                }
+
+                if(fragment != null)
+                    getFragmentManager().beginTransaction().replace(R.id.main_fragment_content, fragment).commit();
+
+                return true;
+            }
+        };
 }
