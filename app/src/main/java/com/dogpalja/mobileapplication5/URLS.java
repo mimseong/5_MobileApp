ff588fe22b36417154ce3d365e724ef1750418cb
mimseong <parkseongmin98@gmail.com>
2019-12-10 22:58:31 +0900

    URL 추가

--- app/src/main/java/com/dogpalja/mobileapplication5/URLS.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/URLS.java
@@ -5,4 +5,5 @@ public class URLS {
     static final String sign_up_api = "https://mobileapp5.000webhostapp.com/sign_up.php";
     static final String upload_story_image = "https://mobileapp5.000webhostapp.com/upload_story_image.php";
     static final String get_user_data = "https://mobileapp5.000webhostapp.com/get_user_data.php?user_id=";
+    static final String get_all_images = "http://mobileapp5.000webhostapp.com/get_all_images.php?id=";
 }
