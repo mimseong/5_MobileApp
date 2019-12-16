3bef6ae1dfd247addd95876b9ca58131c3ebcb2f
mimseong <parkseongmin98@gmail.com>
2019-12-16 17:14:36 +0900

    안쓰는 디비관련 클래스 삭제

--- app/src/main/java/com/dogpalja/mobileapplication5/URLS.java
+++
@@ -1,9 +0,0 @@
-package com.dogpalja.mobileapplication5;
-
-public class URLS {
-    static final String login_api = "https://mobileapp5.000webhostapp.com/login.php";
-    static final String sign_up_api = "https://mobileapp5.000webhostapp.com/sign_up.php";
-    static final String upload_story_image = "https://mobileapp5.000webhostapp.com/upload_story_image.php";
-    static final String get_user_data = "https://mobileapp5.000webhostapp.com/get_user_data.php?user_id=";
-    static final String get_all_images = "http://mobileapp5.000webhostapp.com/get_all_images.php?id=";
-}
