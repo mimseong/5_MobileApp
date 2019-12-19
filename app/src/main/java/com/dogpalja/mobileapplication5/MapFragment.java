bf09fa1bca19863ab1836add51e2f3372e82fb49
mimseong <parkseongmin98@gmail.com>
2019-12-20 05:42:18 +1300

    안쓰는 함수 삭제

--- app/src/main/java/com/dogpalja/mobileapplication5/MapFragment.java
+++ app/src/main/java/com/dogpalja/mobileapplication5/MapFragment.java
@@ -65,20 +65,13 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     Vector<Double> latitudeV;
     Vector<Double> longitudeV;
 
-    public MapFragment() {
-        // Required empty public constructor
-    }
-
-
+    public MapFragment() { }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //내위치 받아오는 부분 선언
-        //https://vvh-avv.tistory.com/138 << 여기서 봤다.
 
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -183,27 +176,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener(){
             @Override
             public void onMapClick(LatLng point) {
-                final MarkerOptions mOptions = new MarkerOptions();
-                // 마커 타이틀
-                mOptions.title("대표사진보기");
-                mOptions.position(new LatLng(point.latitude, point.longitude));
-                // 마커(핀) 추가 , 맵 클릭시 확인창 띄우기
-                AlertDialog.Builder dialog = new AlertDialog.Builder(MapFragment.this.getActivity());
-                dialog.setTitle("가봤던 곳").setMessage("핀을 추가 하시겠습니까?")
-                        .setPositiveButton("네", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Log.i("clis", "마커 추가");
-                                googleMap.addMarker(mOptions); // 마커 추가.
-                                Toast.makeText(MapFragment.this.getActivity(), "추가 하였습니다.", Toast.LENGTH_SHORT).show();
-                            }
-                        }).setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.i("clis", "클릭했습니다.");
-                        Toast.makeText(MapFragment.this.getActivity(), "추가 하지 않았습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                }).create().show();
             }
         });
 
@@ -220,28 +192,6 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
         googleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(final Marker marker) {
-                //삭제 할껀지... 여쭈어봄..
-                final MarkerOptions mOptions = new MarkerOptions();
-                // 마커 타이틀
-                mOptions.title("삭제");
-                // 마커(핀) 추가 , 맵 클릭시 확인창 띄우기
-                AlertDialog.Builder dialog = new AlertDialog.Builder(MapFragment.this.getActivity());
-                dialog.setTitle("여쭈어봄").setMessage("삭제하시겠습니까?")
-                        .setPositiveButton("네", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Log.i("clis", "마커 추가");
-                                marker.remove(); // 마커 삭제.
-                                Toast.makeText(MapFragment.this.getActivity(), "삭제 하였습니다.", Toast.LENGTH_SHORT).show();
-                            }
-                        }).setNegativeButton("아니오", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Log.i("clis", "클릭했습니다.");
-                        Toast.makeText(MapFragment.this.getActivity(), "삭제 하지 않았습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                }).create().show();
-                Log.i("clis", "마커 클릭");
                 return false;
             }
         });
